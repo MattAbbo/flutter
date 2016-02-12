6d9f9e3c8355a03fd6ce4b2e0766470006dc3246
Matt Aberdein <matt@snapscan.co.za>
2016-02-12 16:06:16 +0200

    add pages controller, pages: index, explore, home, profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'home' => 'pages#home'
+  get 'profile' => 'pages#profile'
+  get 'explore' => 'pages#explore'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
